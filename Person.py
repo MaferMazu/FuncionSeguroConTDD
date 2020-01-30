7eb75c3051fc803fe6b990ddced0d1f1db76469f
María Fernanda Magallanes <35668326+MaferMazu@users.noreply.github.com>
2020-01-30 12:23:54 -0400

    Class Person

---
+++ Person.py
@@ -0,0 +1,14 @@
+import datetime
+
+class Person:
+    def __init__(self, name, genre, birth, quotation):
+        self.name = name
+        self.birth = birth
+        self.genre = genre
+        self.quotation = quotation
+    
+    def __str__(self):
+        return self.name + " " + self.genre + " " + str(self.birth.year) + " " + str(self.quotation)
+
+    def AbleIVSSPension(self):
+        pass
---
+++ test.py
@@ -0,0 +1,12 @@
+import unittest
+import datetime
+from Person import Person
+
+class TestPension(unittest.TestCase):
+    def TestPensionMale60And750Quot(self)
+        date = datetime.datetime(1996, 2, 13)
+        p=Person("Pedro","M",date,750)
+        p.AbleIVSSPension()
+
+if __name__ == '__main__':
+    unittest.main()
